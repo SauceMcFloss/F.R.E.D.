aee1e294658b4869780981fc5afee5964dbb0de4
devindraizin <devindraizin@knights.ucf.edu>
2019-04-05 13:43:03 -0400

    Added autonomous function

--- Code/src/UI/autonomousUI/autonomousScreenController.java
+++ Code/src/UI/autonomousUI/autonomousScreenController.java
@@ -1,6 +1,46 @@
 package UI.autonomousUI;
 
+import NeuralNetwork.TrafficSignDetectFX;
+import arduinoControl.Car;
+import com.jfoenix.controls.JFXToggleButton;
+import javafx.fxml.FXML;
+
 public class autonomousScreenController
 {
 
+    @FXML
+    JFXToggleButton toggleButton;
+
+    public void initialize()
+    {
+
+    }
+
+    private void startStop()
+    {
+
+    }
+
+    public void toggleAutonomousMode()
+    {
+
+        if(toggleButton.isSelected())
+        {
+            Car.getInstance().moveCarForward();
+
+            while(toggleButton.isSelected())
+            {
+                if(TrafficSignDetectFX.signFound)
+                {
+                    Car.getInstance().setCarNeutral();
+                }
+
+            }
+        }
+        else
+        {
+            Car.getInstance().setCarNeutral();
+        }
+
+    }
 }
