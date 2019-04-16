60d8083155f4580c9816e007aaf02f9f2a581416
devindraizin <devindraizin@knights.ucf.edu>
2019-04-16 13:16:31 -0400

    Fixed hazard detection

--- Code/src/UI/autonomousUI/autonomousScreenController.java
+++ Code/src/UI/autonomousUI/autonomousScreenController.java
@@ -11,14 +11,36 @@ public class autonomousScreenController
     @FXML
     JFXToggleButton toggleButton;
 
+    private static volatile boolean hasHazardDetection = false;
+
     public void initialize()
     {
 
     }
 
-    private void Stop()
+    private void beginHazardDetection()
     {
+        new Thread(() -> {
 
+            while(hasHazardDetection)
+            {
+               if(Car.distanceToSign < 150)
+               {
+                   Car.getInstance().setCarNeutral();
+               }
+               else
+               {
+                   Car.getInstance().moveCarForward();
+               }
+            }
+
+
+        }).start();
+    }
+
+    private void stopHazardDetection()
+    {
+        hasHazardDetection = false;
     }
 
     public void toggleAutonomousMode()
@@ -26,11 +48,13 @@ public class autonomousScreenController
 
         if(toggleButton.isSelected())
         {
+            beginHazardDetection();
         	Car.getInstance().setAutonomousMode(true);
             Car.getInstance().moveCarForward();
         }
         else
         {
+            stopHazardDetection();
         	Car.getInstance().setAutonomousMode(false);
             Car.getInstance().setCarNeutral();
         }
