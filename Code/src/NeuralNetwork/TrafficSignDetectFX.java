640af672e10839f8f84d3ee64fbab0b1fed5e67a
devindraizin <devindraizin@knights.ucf.edu>
2019-04-16 12:48:07 -0400

    Fixed issue with autonomous threading

--- Code/src/NeuralNetwork/TrafficSignDetectFX.java
+++ Code/src/NeuralNetwork/TrafficSignDetectFX.java
@@ -20,7 +20,7 @@ import javafx.scene.image.*;
 
 /*
  * This class starts the camera and processes the frames seen by
- * capturing a frame, processing it with the classifier, and 
+ * capturing a frame, processing it with the classifier, and
  * creating a matrix of Rects and based on the size of the
  * array we can tell if a sign was found. Then the image is then
  * updated to be displayed.
@@ -28,106 +28,126 @@ import javafx.scene.image.*;
 
 public class TrafficSignDetectFX {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private DaemonThread myThread = null;
-	private VideoCapture webSource = null;
-	private Mat frame;
-	private MatOfByte mem;
-	private CascadeClassifier signDetect;
-	private Image image;
-	private ImageView display;
-	private MatOfRect signDetections;
-	public static volatile boolean signFound = false;
+    private DaemonThread myThread = null;
+    private VideoCapture webSource = null;
+    private Mat frame;
+    private MatOfByte mem;
+    private CascadeClassifier signDetect;
+    private Image image;
+    private ImageView display;
+    private MatOfRect signDetections;
+    public static volatile boolean signFound = false;
 
-	public ImageView getDisplay() {
-		return display;
-	}
+    public ImageView getDisplay() {
+        return display;
+    }
 
-	public void setDisplay(ImageView display) {
-		this.display = display;
-	}
+    public void setDisplay(ImageView display) {
+        this.display = display;
+    }
 
-	public TrafficSignDetectFX() {
-		webSource = new VideoCapture(
-				"http://192.168.1.103:32805/video"); /*
-													 * Sets where to get footage from "0":front facing cam "1":secondary
-													 * "http://.../video": IP stream
-													 */
-		myThread = new DaemonThread();
-		Thread t = new Thread(myThread);
-		t.setDaemon(true);
-		myThread.runnable = true;
-		frame = new Mat();
-		mem = new MatOfByte();
-		signDetect = new CascadeClassifier("C:/opencv/build/etc/haarcascades/stopsign_classifier.xml");// Loads
-																										// classifier
-		signDetections = new MatOfRect();
-		t.start(); // start thread
-	}
 
-	class DaemonThread implements Runnable {
-		protected volatile boolean runnable = false;
+    public TrafficSignDetectFX() {
+        webSource = new VideoCapture(
+                "http://192.168.1.100:32805/video"); /*
+         * Sets where to get footage from "0":front facing cam "1":secondary
+         * "http://.../video": IP stream
+         */
+        myThread = new DaemonThread();
+        Thread t = new Thread(myThread);
+        t.setDaemon(true);
+        myThread.runnable = true;
+        frame = new Mat();
+        mem = new MatOfByte();
+        signDetect = new CascadeClassifier("C:/opencv/build/etc/haarcascades/stopsign_classifier.xml");// Loads
+        // classifier
+        signDetections = new MatOfRect();
+        t.start(); // start thread
+    }
 
-		public void run() {
-			int flag = 0;
-			int count = 0;
-			synchronized (this) {
+    class DaemonThread implements Runnable {
 
-				while (runnable) {
-					if (webSource.grab()) {
-						try {
-							webSource.retrieve(frame);
-							signDetect.detectMultiScale(frame, signDetections, 1.05, 4, 0, new Size(30, 30),
-									new Size());
 
-							if (signDetections.toArray().length == 0) {// If no signs in view
-								signFound = false;
-							}
+        private void sendStopWithDelay(int time)
+        {
 
-							if (signDetections.toArray().length > 0) {// If sign was found in view
-								if (!signFound) {// If first time frame sign seen
-									System.out.println("STOP CAR");
+            Car.getInstance().setCarNeutral();
 
-									signFound = true;
+            new Thread(() ->
+            {
+                try {
+                    Thread.sleep(time);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            });
 
-									if (Car.getInstance().getAutonomousMode()) {
-										Car.getInstance().setCarNeutral();
-										
-										flag = 1;
-										count = 50;
+            Car.getInstance().moveCarForward();
 
-									}
 
-								}
-								// Draws borders around the sign/signs
-								for (Rect rect : signDetections.toArray()) {
-									Imgproc.rectangle(frame, rect.tl(), rect.br(), new Scalar(0, 255, 0));
-								}
-								
-							}
-							// Processes the image to be displayed through ImageView
-							Imgcodecs.imencode(".bmp", frame, mem);
-							image = new Image(new ByteArrayInputStream(mem.toArray()));
-							display.setImage(image);
-							
-							if (flag == 1){
-								
-								if (count>0) {
-									count--;
-								}
-								else {
-									flag = 0;
-									Car.getInstance().moveCarForward();
-								}
-							}
+        }
 
-						} catch (Exception ex) {
-							ex.printStackTrace();
-						}
-					}
-				}
-			}
-		}
-	}
+        protected volatile boolean runnable = false;
+
+        public void run() {
+            int flag = 0;
+            int count = 0;
+            synchronized (this) {
+
+                while (runnable) {
+                    if (webSource.grab()) {
+                        try {
+                            webSource.retrieve(frame);
+                            signDetect.detectMultiScale(frame, signDetections, 1.05, 4, 0, new Size(30, 30),
+                                    new Size());
+
+                            if (signDetections.toArray().length == 0) {// If no signs in view
+                                signFound = false;
+                            }
+
+                            if (signDetections.toArray().length > 0) {// If sign was found in view
+                                if (!signFound) {// If first time frame sign seen
+                                    System.out.println("STOP CAR");
+
+                                    signFound = true;
+
+                                    if (Car.getInstance().getAutonomousMode()) {
+
+                                        sendStopWithDelay(3000);
+
+                                    }
+
+                                }
+                                // Draws borders around the sign/signs
+                                for (Rect rect : signDetections.toArray()) {
+                                    Imgproc.rectangle(frame, rect.tl(), rect.br(), new Scalar(0, 255, 0));
+                                }
+
+                            }
+                            // Processes the image to be displayed through ImageView
+                            Imgcodecs.imencode(".bmp", frame, mem);
+                            image = new Image(new ByteArrayInputStream(mem.toArray()));
+                            display.setImage(image);
+
+                            if (flag == 1){
+
+                                if (count>0) {
+                                    count--;
+                                }
+                                else {
+                                    flag = 0;
+                                    Car.getInstance().moveCarForward();
+                                }
+                            }
+
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
